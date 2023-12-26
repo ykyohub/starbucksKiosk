34b1eaa5d3bf81ecd8e1a506f846803ef6e83e10
yktohub <rbgh2858@naver.com>
2023-12-26 15:25:58 +0900

    Initial Commit

---
+++ starbucksKiosk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  starbucksKiosk
+//
+//  Created by 윤규호 on 12/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
