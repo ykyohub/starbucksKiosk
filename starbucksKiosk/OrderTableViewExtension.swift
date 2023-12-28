5765701ff03ccd4e5a17e6fcdae2c0537f378c19
yktohub <rbgh2858@naver.com>
2023-12-28 21:17:20 +0900

    feat: collection view <-> table view 연동

--- starbucksKiosk/OrderTableViewExtension.swift
+++ starbucksKiosk/OrderTableViewExtension.swift
@@ -12,17 +12,17 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cart.count
+        return customer.cart.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = OrderTableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath) as! OrderTableViewCell
         
-        cell.setCell(cart[indexPath.row])
+        cell.setCell(customer.cart[indexPath.row])
         
         cell.orderDeleteButton = { [weak self] in
             print(indexPath.row)
-            self?.cart.remove(at: indexPath.row)
+            self?.customer.cart.remove(at: indexPath.row)
             self?.OrderTableView.deleteRows(at: [indexPath], with: .automatic)
             self?.OrderTableView.reloadData()
         }
