349ab3df5458f510687b59184817d04abfb65d3b
yktohub <rbgh2858@naver.com>
2024-01-03 21:24:19 +0900

    fix : tableview cell 내부 count 변수값 uibutton -> uistepper 로 변경 및 기능구현

--- starbucksKiosk/OrderTableViewCell.swift
+++ starbucksKiosk/OrderTableViewCell.swift
@@ -14,6 +14,9 @@ class OrderTableViewCell: UITableViewCell {
     var orderDeleteButton: (() -> ())?
     var decreaseButton: (() -> ())?
     var increaseButton: (() -> ())?
+    var orderChange: ((Int) -> ())?
+    @IBOutlet weak var tableViewStepper: UIStepper!
+    
     var menu: Menu?
     
     @IBOutlet weak var orderTableViewProductLabel: UILabel!
@@ -21,31 +24,37 @@ class OrderTableViewCell: UITableViewCell {
     @IBOutlet weak var orderTableViewCountLabel: UILabel!
     @IBOutlet weak var orderTableViewImage: UIImageView!
     
-    
-    @IBAction func orderTableViewDeleteButton(_ sender: UIButton) {
+    @IBAction func tableViewStepper(_ sender: UIStepper) {
+        print("스테퍼 동작, sender.value : ", sender.value)
+        orderChange?(Int(sender.value))
+    }
+    @IBAction func orderTableViewDeleteButton(_ sender: UIButton){
         orderDeleteButton?()
     }
-    @IBAction func orderTableViewDecreaseButton(_ sender: UIButton) {
-        decreaseButton?()
-    }
-    @IBAction func orderTableViewIncreaseButton(_ sender: UIButton) {
-        increaseButton?()
-    }
+
     
     func setCell(_ _menu: Menu){
         menu = _menu
         
+        self.tableViewStepper.value = Double(_menu.number)
+        print("setCell 메서드 실행, 스테퍼 벨류 : ", tableViewStepper.value)
         self.orderTableViewProductLabel.text = _menu.name
         self.orderTableViewPriceLabel.text = String(_menu.price) + " 원"
         self.orderTableViewCountLabel.text = String(_menu.number)
         self.orderTableViewImage.image = UIImage(named: _menu.image)
-        
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        print("prepareForReuse 메서드 실행됨")
+        self.tableViewStepper.value = 0
+        
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
--- starbucksKiosk/OrderTableViewExtension.swift
+++ starbucksKiosk/OrderTableViewExtension.swift
@@ -21,20 +21,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         // 고객 장바구니에 들어간 메뉴로 TableViewCell 구성
         cell.setCell(customer.cart[indexPath.row])
         
-        // 주문내역 감소 버튼 동작
-        cell.decreaseButton = { [weak self] in
-            if self?.customer.cart[indexPath.row].number != 0 {
-                self?.customer.cart[indexPath.row].number -= 1
-            }
-            self?.OrderTableView.reloadData()
-            self?.customer.calculateTotal()
-            self?.totalPriceLabel.text = String(self?.customer.totalPrice ?? 0) + " 원"
-            self?.totalQuantityLabel.text = String(self?.customer.totalQuantity ?? 0) + " 개"
-        }
-        
-        // 주문내역 증가 버튼 동작
-        cell.increaseButton = { [weak self] in
-            self?.customer.cart[indexPath.row].number += 1
+        cell.orderChange = { [weak self] value in
+            self?.customer.cart[indexPath.row].number = value
+            print("장바구니 넘버: ",self?.customer.cart[indexPath.row].number ?? 0)
             self?.OrderTableView.reloadData()
             self?.customer.calculateTotal()
             self?.totalPriceLabel.text = String(self?.customer.totalPrice ?? 0) + " 원"
@@ -43,6 +32,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         
         // 셀 내부 쓰레기통 버튼 기능
         cell.orderDeleteButton = { [weak self] in
+            
             self?.customer.cart.remove(at: indexPath.row)
             self?.OrderTableView.deleteRows(at: [indexPath], with: .left)
             self?.OrderTableView.reloadData()
